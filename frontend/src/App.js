bb915e0c4aeb6a0648cb3f31c987670ee5fe0e78
ratetanginamo <168286942+ratetanginamo@users.noreply.github.com>
2025-09-19 00:08:10 +0800

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Login />
+    </>
+  );
+}
+
+export default App;
